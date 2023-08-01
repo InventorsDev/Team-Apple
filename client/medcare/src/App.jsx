71f039eb7d45be3e8e217101367bb47e1b9a092f
Williamsolamide <117740544+Williamsolamide@users.noreply.github.com>
2023-08-01 22:42:37 +0100

    changed client to web

---
+++ client/medcare/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+
+function App() {
+ 
+
+  return (
+    <>
+    <Header/>
+    <div className="text-xs border-1 border-red-500 bg-blue-500">Hello World</div>
+    <Header/>
+    <Header/>
+    <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ client/medcare/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+
+    return (
+        <div ><div>Header</div><div>Williams</div></div>
+  )
+}
+
+export default Header
